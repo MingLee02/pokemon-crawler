1afe44009a91ff4c734b13e02ab10bfe672b83be
ming lee <ming.lee@hotmail.com>
2021-08-08 18:52:03 +0100

    move code into smaller function.

--- pokemon/utils.py
+++ pokemon/utils.py
@@ -7,6 +7,33 @@ from pokemon.models import pokedexVersion, pokemon, pokemonVersionDescription
 from pokemon.management.commands.command_utils import checkUrl
 
 
+def create_pokemon_entry(record):
+    pokemon_obj , _ = pokemon.objects.get_or_create(
+        pokedex_id=record.get('id')
+    )
+    pokemon_types = record.get('types')
+
+    image_response = checkUrl(record.get('sprites').get('front_default'))
+
+    data =  ContentFile(image_response.content)
+    stats = record.get('stats')
+
+    pokemon_obj.name = record.get('name')
+    pokemon_obj.type_one = pokemon_types[0]['type']['name']
+    if len(pokemon_types) == 2:
+        pokemon_obj.type_two = pokemon_types[1].get('type').get('name', None)
+    pokemon_obj.sprite.save(record.get('name'), data)
+    pokemon_obj.weight = record.get('weight')
+    pokemon_obj.height = record.get('height')
+    pokemon_obj.health_points = stats[0]['base_stat']
+    pokemon_obj.attack = stats[1]['base_stat']
+    pokemon_obj.defence = stats[2]['base_stat']
+    pokemon_obj.speed = stats[5]['base_stat']
+    pokemon_obj.save()
+
+    return pokemon_obj
+
+
 def obtain_pokemon(version='blue', start='scratch', limit=10):
     version = pokedexVersion.objects.filter(name=version)
     skipped = False
@@ -17,6 +44,7 @@ def obtain_pokemon(version='blue', start='scratch', limit=10):
         else:
             start_num = pokemon.objects.all().count() + 1
             skipped = pokemon.objects.last()
+            
         for count in range(start_num, limit):
             url = "https://pokeapi.co/api/v2/pokemon/{}".format(count)
             response = checkUrl(url)
@@ -25,29 +53,7 @@ def obtain_pokemon(version='blue', start='scratch', limit=10):
 
             records = json.loads(response.text)
             if records:
-                pokemon_obj , _ = pokemon.objects.get_or_create(
-                    pokedex_id=records.get('id')
-                )
-                pokemon_types = records.get('types')
-
-                image_response = checkUrl(records.get('sprites').get('front_default'))
-            
-                data =  ContentFile(image_response.content)
-                stats = records.get('stats')
-
-                pokemon_obj.name = records.get('name')
-                pokemon_obj.type_one = pokemon_types[0]['type']['name']
-                if len(pokemon_types) == 2:
-                    pokemon_obj.type_two = pokemon_types[1].get('type').get('name', None)
-                pokemon_obj.sprite.save(records.get('name'), data)
-                pokemon_obj.weight = records.get('weight')
-                pokemon_obj.height = records.get('height')
-                pokemon_obj.health_points = stats[0]['base_stat']
-                pokemon_obj.attack = stats[1]['base_stat']
-                pokemon_obj.defence = stats[2]['base_stat']
-                pokemon_obj.speed = stats[5]['base_stat']
-
-                pokemon_obj.save()
+                pokemon_obj = create_pokemon_entry(records)
 
                 species_url = 'https://pokeapi.co/api/v2/pokemon-species/{}'.format(pokemon_obj.name)
                 response = checkUrl(species_url)
