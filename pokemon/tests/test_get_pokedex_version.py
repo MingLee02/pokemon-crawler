abb2cb1dfd138690aa6a1c2f1b573a55707b97ab
ming lee <ming.lee@hotmail.com>
2021-08-08 16:07:09 +0100

    Add test.

--- pokemon/tests/test_get_pokedex_version.py
+++ pokemon/tests/test_get_pokedex_version.py
@@ -1,9 +1,25 @@
-from requests import HTTPError
+from io import StringIO
 
-from pokemon.management.commands.get_pokedex_version import checkUrl
+from django.core.management import call_command
+from django.test import TestCase
+
+from pokemon.models import pokedexVersion
 
 
-def test_checkUrl_fail(requests_mock):
-    requests_mock.get('pokemon_url_somthing', status_code=404)
-    response = checkUrl('pokemon_url_somthing')
-    assert response == 'Error'
+class GetVersionsTestCase(TestCase):
+    def call_command(self, *args, **kwargs):
+        out = StringIO()
+        call_command(
+            "get_pokedex_version",
+            *args,
+            stdout=out,
+            stderr=StringIO(),
+            **kwargs,
+        )
+        return out.getvalue()
+    
+    def test_write(self):
+        assert pokedexVersion.objects.filter(name='blue').count() == 0
+        out = self.call_command()
+
+        assert pokedexVersion.objects.filter(name='blue').count() == 1
