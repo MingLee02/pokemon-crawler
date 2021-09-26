2ef28865aaa63f42ee6bf3073a9fce487e281cbc
ming lee <ming.lee@hotmail.com>
2021-09-26 14:47:48 +0100

    Teak page 2

--- pokemon/forms.py
+++ pokemon/forms.py
@@ -5,3 +5,8 @@ from pokemon.models import pokedexVersion
 
 class PokedexSelectForm(forms.Form):
     pokedex_choice = forms.ModelChoiceField(queryset=pokedexVersion.objects.exclude(pokemonversiondescription__version__isnull=True))
+
+    def __init__(self, *args, **kwargs):
+        super(PokedexSelectForm, self).__init__(*args, **kwargs)
+        print(self.fields['pokedex_choice'].__dict__)
+        print(dir(self.fields['pokedex_choice']))
